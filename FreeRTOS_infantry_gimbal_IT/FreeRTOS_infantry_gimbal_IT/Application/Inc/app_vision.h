e7ca683b876eafcafc87c6fbdb12d7f1dcfd1813
YALDZJC <152235531+YALDZJC@users.noreply.github.com>
2024-08-21 19:14:05 +0800

    上传云端

---
+++ FreeRTOS_infantry_gimbal_IT/FreeRTOS_infantry_gimbal_IT/Application/Inc/app_vision.h
@@ -0,0 +1,26 @@
+#ifdef APP_VISION_GLOBALS
+#define APP_VISION_EXTERN
+#else
+#define APP_VISION_EXTERN extern
+#endif
+
+typedef struct{
+	float vision_pitch_increment_angle;
+	float vision_yaw_increment_angle;
+}vision_struct_t;
+
+APP_VISION_EXTERN uint8_t vision_fire;
+APP_VISION_EXTERN uint8_t vision_ready;
+
+APP_VISION_EXTERN uint8_t vision_send_buf[12];
+
+APP_VISION_EXTERN vision_struct_t vision_struct;
+
+/* 视觉调试标识符 */
+APP_VISION_EXTERN uint8_t vision_enable , windmill_enable;//视觉启动，能量机关启动
+
+APP_VISION_EXTERN uint8_t bullet_speed ;
+
+void vision_recive_task( void *pvParameters );
+void vision_send_task( void *pvParameters );
+void receive_task();
